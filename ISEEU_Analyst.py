0d8121fa07606bbe7a63393a206a39392face031
labyrenth <ak5690@naver.com>
2019-01-15 22:16:16 +0900

    ISEEU_Interface_0.1v

---
+++ ISEEU_Analyst.py
@@ -0,0 +1,7 @@
+#Analysis 모듈을 모아놓은 python 파일.
+
+def Packet_Analysis():
+    print("DDOS_SCAN!")
+
+def Firmware_Analysis():
+    print("Firmware_Analysis")
---
+++ ISEEU_Scanner.py
@@ -0,0 +1,22 @@
+#Scanner 모듈을 모아놓은 python 파일.
+
+def Port_Scan():
+    print("Port_Scan!")
+
+def Ssh_Scan():
+    print("SSH_Scan!")
+
+def Telnet_Scan():
+    print("Telnet_Scan!")
+
+def Http_Scan():
+    print("HTTP_SCAN!")
+
+def Packet_Scan():
+    print("Packet_SCAN!")
+
+def Backdoor_Scan():
+    print("Backdoor_SCAN!")
+
+def Firmware_Scan():
+    print("Firmware_SCAN!")
